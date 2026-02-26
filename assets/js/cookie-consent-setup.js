fc8bbe3fc4ef1921b8dc5f800d4a750163eed0fa
phd-benel <82882383+phd-benel@users.noreply.github.com>
2026-02-26 16:23:03 +0000

    Deploying to gh-pages from @ phd-benel/phd-benel.github.io@56b66f5a0210de9d40ef6d4244b2b65c3f078c11 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://phd-benel.github.io/phd-benel.github.io//#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://phd-benel.github.io/#contact">contact us</a>.'
               }
             ]
           }
